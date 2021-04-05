b920231a60237f0b32f1a80d442e3d2dfd84e683
tkk <tokiko.develop@gmail.com>
2021-04-06 02:12:56 +0900

    Fix: remove previous version go

--- go_installer.sh
+++ go_installer.sh
@@ -4,4 +4,5 @@ VERSION="${1:-1.14.4}"
 OS=${2:-linux}
 ARCH=${3:-amd64}
 curl -L https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz -o /tmp/go$VERSION.$OS-$ARCH.tar.gz
+sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf /tmp/go$VERSION.$OS-$ARCH.tar.gz
