413d59c73a5149738a178cccfd42be1f87c746fe
tkk <tokiko.develop@gmail.com>
2022-04-15 16:25:08 +0900

    Use packer.nvim

--- init.sh
+++ init.sh
@@ -57,5 +57,5 @@ fi
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ## nvim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
