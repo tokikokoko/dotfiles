d561ffece27e1d3a57c0ee95adc77c35171127de
tkk <tokiko.develop@gmail.com>
2022-08-01 15:15:41 +0900

    Use fzf

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -42,8 +42,8 @@ vim.api.nvim_set_keymap('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<CR
 
 vim.cmd([[nnoremap <Leader>fh :noh<CR>]])
 
-vim.cmd([[nnoremap <Leader>pf :Telescope find_files hidden=true<CR>]])
-vim.cmd([[nnoremap <Leader>bb :Telescope buffers<CR>]])
+vim.cmd([[nnoremap <Leader>pf :lua require('fzf-commands').files()<CR>]])
+vim.cmd([[nnoremap <Leader>bb :lua require('fzf-commands').bufferpicker()<CR>]])
 
 
 vim.cmd([[tnoremap <Esc> <C-\><C-n>]])
