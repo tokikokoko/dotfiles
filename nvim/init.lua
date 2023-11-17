7d14ca59f3b1b72c2a218052c2222328ab34956c
tkk <tokiko.develop@gmail.com>
2023-11-17 18:36:37 +0900

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -41,11 +41,12 @@ vim.diagnostic.config({
 -- vim.opt.listchars:append "space:⋅"
 -- vim.opt.listchars:append "eol:↴"
 
-require("indent_blankline").setup {
-  show_end_of_line = true,
-  show_current_context = true,
-  show_current_context_start = true,
-}
+require("ibl").setup()
+-- require("ibl").setup {
+--   show_end_of_line = true,
+--   show_current_context = true,
+--   show_current_context_start = true,
+-- }
 
 -- Git
 require('git').setup({
